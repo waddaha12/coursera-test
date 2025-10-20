1cfca97fbae3fe08f339e1dbebc2582f40572ca1
waddaha sami <wadahataha2002@gmail.com>
2025-10-20 23:36:36 +0300

    Create playergui.h

---
+++ playergui.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
+
+class PlayerGUI : public juce::Component,
+                  public juce::Button::Listener,
+                  public juce::Slider::Listener
+{
+public:
+    PlayerGUI();
+    ~PlayerGUI() override;
+
+    void resized() override;
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+
+private:
+    PlayerAudio playerAudio;
+
+    juce::TextButton loadButton{ "Load" };
+    juce::TextButton playButton{ "Play" };
+    juce::TextButton stopButton{ "Stop" };
+    juce::TextButton restartButton{ "Restart" };
+    juce::Slider volumeSlider;
+
+    std::unique_ptr<juce::FileChooser> chooser;
+
+    void buttonClicked(juce::Button* button) override;
+    void sliderValueChanged(juce::Slider* slider) override;
+};
