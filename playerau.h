f33e2439e8510aecb225a4a9cce230625cc747d2
waddaha sami <wadahataha2002@gmail.com>
2025-10-21 00:01:44 +0300

    Update playerau.h

--- playerau.h
+++ playerau.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <JuceHeader.h>
 
+// PlayerAudio: handles loading, playback, gain, position, and looping.
 class PlayerAudio
 {
 public:
@@ -15,14 +16,25 @@ public:
     void play();
     void stop();
     void restart();
-    void setGain(float gain);
 
+    void setGain(float gain);
     double getPosition() const;
     void setPosition(double pos);
     double getLength() const;
 
+    // Loop control
+    void setLooping(bool shouldLoop);
+    bool isLooping() const;
+
 private:
     juce::AudioFormatManager formatManager;
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+
+    // We keep readerSource as a PositionableAudioSource unique_ptr (AudioFormatReaderSource derives from that)
+    std::unique_ptr<juce::PositionableAudioSource> readerSource;
+
+    // LoopingAudioSource wraps the readerSource to provide looping behavior.
+    std::unique_ptr<juce::LoopingAudioSource> loopingSource;
+
     juce::AudioTransportSource transportSource;
+    bool loopingEnabled { false };
 };
